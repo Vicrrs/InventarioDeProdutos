bd33b612d039682a091b6a882197a0b3a1a8b612
Vicrrs <enricofermi20192@gmail.com>
2024-04-02 02:02:08 -0300

    variaveis de ambiente

--- db/db.go
+++ db/db.go
@@ -2,13 +2,22 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
 
-	_ "github.com/lib/pq"
+	_ "github.com/lib/pq" // Importação do driver PostgreSQL
 )
 
 func ConectaComBancoDeDados() *sql.DB {
-	conn := "user=postgres dbname=postgres password=admin@123 host=localhost sslmode=disable"
-	db, err := sql.Open("postgres", conn)
+	user := os.Getenv("DB_USER")
+	dbname := os.Getenv("DB_NAME")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	connStr := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=%s", user, dbname, password, host, sslmode)
+
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		panic(err.Error())
 	}
