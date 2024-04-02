21c0be75582f478bccf7907dc1300e28253ab9d0
Vicrrs <enricofermi20192@gmail.com>
2024-04-01 22:30:48 -0300

    struct Produto

--- main.go
+++ main.go
@@ -5,6 +5,14 @@ import (
 	"text/template"
 )
 
+// Colecao de variaveis que formam um novo tipo
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -13,5 +21,13 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 59, Quantidade: 5},
+		{"Tênis", "Confortável", 89.9, 3},
+		{"Fone", "Muito bom", 129.0, 2},
+		{"Notebook", "Acer", 5999.0, 4},
+	}
+
+	temp.ExecuteTemplate(w, "Index", produtos)
 }
