bfce1e220b2072fb50c96905afd10be91cef1967
Vicrrs <enricofermi20192@gmail.com>
2024-04-02 01:55:10 -0300

    modularizando o projeto

--- main.go
+++ main.go
@@ -1,30 +1,12 @@
 package main
 
 import (
-	"database/sql"
 	"net/http"
 	"text/template"
 
+	"github.com/Vicrrs/InventarioDeProdutos/models"
 	_ "github.com/lib/pq"
 )
-
-func conectaComBancoDeDados() *sql.DB {
-	conn := "user=postgres dbname=postgres password=admin@123 host=localhost sslmode=disable"
-	db, err := sql.Open("postgres", conn)
-	if err != nil {
-		panic(err.Error())
-	}
-	return db
-}
-
-// Colecao de variaveis que formam um novo tipo
-type Produto struct {
-	Id         int
-	Nome       string
-	Descricao  string
-	Preco      float64
-	Quantidade int
-}
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
@@ -34,35 +16,7 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
+	todosOsProdutos := models.BuscaTodosOsProdutos()
+	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
 
-	db := conectaComBancoDeDados()
-
-	selecDeTodosOsProdutos, err := db.Query("SELECT * FROM produtos")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	p := Produto{}
-	produtos := []Produto{}
-
-	for selecDeTodosOsProdutos.Next() {
-		var id, quantidade int
-		var nome, descricao string
-		var preco float64
-
-		err := selecDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
-		if err != nil {
-			panic(err.Error())
-		}
-
-		p.Nome = nome
-		p.Descricao = descricao
-		p.Preco = preco
-		p.Quantidade = quantidade
-
-		produtos = append(produtos, p)
-	}
-
-	temp.ExecuteTemplate(w, "Index", produtos)
-	defer db.Close()
 }
