959727e3b085c384c40f61e0ee296dee5f2d2984
Vicrrs <enricofermi2019@gmail.com>
2024-04-26 18:51:43 -0400

    mudando a maneira de conexao para pegar as credenciais do .env

--- db/db.go
+++ db/db.go
@@ -8,22 +8,20 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// ConectaComBancoDeDados cria e retorna uma conexão com o banco de dados configurado através de variáveis de ambiente.
 func ConectaComBancoDeDados() *sql.DB {
-	// Recupera as variáveis de ambiente necessárias para a conexão com o banco de dados.
 	user := os.Getenv("DB_USER")
 	dbname := os.Getenv("DB_NAME")
 	password := os.Getenv("DB_PASSWORD")
 	host := os.Getenv("DB_HOST")
 	sslmode := os.Getenv("DB_SSLMODE")
 
-	// Formata a string de conexão com os valores das variáveis de ambiente.
-	connStr := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=%s", user, dbname, password, host, sslmode)
+	// Imprime as configurações para verificar
+	fmt.Printf("Conectando com: user=%s dbname=%s password=%s host=%s sslmode=%s\n", user, dbname, password, host, sslmode)
 
-	// Tenta abrir uma conexão com o banco de dados usando a string de conexão formatada.
+	connStr := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=%s", user, dbname, password, host, sslmode)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		panic(err.Error()) // Em caso de erro, encerra a execução com um panic, exibindo a mensagem de erro.
+		panic(err.Error())
 	}
-	return db // Retorna o objeto de conexão com o banco de dados se a conexão for bem-sucedida.
+	return db
 }
