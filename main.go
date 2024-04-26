f0f3145bb753634cb3ef18d1720f5946e443fb33
Vicrrs <enricofermi2019@gmail.com>
2024-04-26 18:52:07 -0400

    importando godotenv

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/Vicrrs/InventarioDeProdutos/routes"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func main() {
+	// Carrega as variáveis de ambiente do arquivo .env
+	err := godotenv.Load() // Esta função procura o arquivo .env na raiz do diretório atual
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
 	routes.CarregaRotas()
-	// Registra a função index como manipuladora da rota raiz.
-	http.ListenAndServe(":8000", nil) // Inicia um servidor HTTP na porta 8000.
+	// Inicia um servidor HTTP na porta 8000.
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
