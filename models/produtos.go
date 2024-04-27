5867cc3f4ba6ec0c911a452c482afee518ae8756
Vicrrs <enricofermi2019@gmail.com>
2024-04-27 18:00:53 -0400

    Botão e funcoes DB para deletar produtos

--- models/produtos.go
+++ models/produtos.go
@@ -36,6 +36,7 @@ func BuscaTodosOsProdutos() []Produto {
 		}
 
 		// Atribui os valores escaneados às propriedades do produto p.
+		p.Id = id
 		p.Nome = nome
 		p.Descricao = descricao
 		p.Preco = preco
@@ -54,5 +55,16 @@ func CriaNovoProduto(nome, descricao string, preco float64, quantidade int) {
 		panic(err.Error())
 	}
 	insereDadosNoBanco.Exec(nome, descricao, preco, quantidade)
+	defer db.Close()
+}
+
+func DeletaProduto(id string) {
+	db := db.ConectaComBancoDeDados()
+
+	deletarOProduto, err := db.Prepare("delete from produtos where id=$1")
+	if err != nil {
+		panic(err.Error())
+	}
+	deletarOProduto.Exec(id)
 	defer db.Close()
 }
